5b2df4080fe2461a637ba8427ee7a28f15c16765
AC <ac787544405@gmail.com>
2017-09-22 10:56:45 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/msi_nb/coolweather/db/City.java
@@ -0,0 +1,44 @@
+package com.example.msi_nb.coolweather.db;
+
+/**
+ * Created by MSI-NB on 2017/9/22.
+ */
+
+public class City {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+}
